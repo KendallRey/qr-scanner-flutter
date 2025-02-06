57b47784f1bc41fa6cb2befaba3fa07c136ffbb9
KendallRey <107897863+KendallRey@users.noreply.github.com>
2025-02-06 16:20:54 +0800

    DATA - added scan item services

---
+++ lib/app/services/scan_item_service.dart
@@ -0,0 +1,30 @@
+import 'package:hive/hive.dart';
+import 'package:qr_scanner/app/core/constants/constants.dart';
+import 'package:qr_scanner/app/core/model/scan_item.dart';
+
+class ScanItemService {
+  Future<Box<dynamic>> getHiveBox() async {
+    final box = await Hive.openBox(Constants.hiveBox);
+    return box;
+  }
+
+  void saveScanItem(ScanItem item) async {
+    final box = await getHiveBox();
+    await box.put(item.id, item);
+  }
+
+  void deleteScanItem(String id) async {
+    final box = await getHiveBox();
+    await box.delete(id);
+  }
+
+  void updateScanItem(ScanItem item) async {
+    final box = await getHiveBox();
+    await box.put(item.id, item);
+  }
+
+  // Stream<BoxEvent> getScannedItems () async* {
+  //   final box = await getHiveBox();
+  //   yield* box.watch();
+  // }
+}
