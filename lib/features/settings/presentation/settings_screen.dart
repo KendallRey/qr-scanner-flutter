333d45ddf46a29c6f7fbd840762777f4f854cc8e
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-02-06 23:29:17 +0800

    SETTINGS - added delete all entries option

--- lib/features/settings/presentation/settings_screen.dart
+++ lib/features/settings/presentation/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:qr_scanner/app/services/scan_item_service.dart';
 
 class SettingsScreenWidget extends StatefulWidget {
@@ -10,14 +11,41 @@ class SettingsScreenWidget extends StatefulWidget {
 
 class _SettingsScreenWidgetState extends State<SettingsScreenWidget> {
   void _handleFlushHive() {
+    if (mounted) {
+      context.pop(true);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('All Items deleted...')));
+    }
     ScanItemService.flushScanItems();
+  }
+
+  Future<bool?> _showDeleteAllDialog() async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Reset ALL'),
+            content:
+                Text("Are you sure you want to delete all saved QR Codes ?"),
+            actions: [
+              TextButton(
+                onPressed: () => context.pop(false),
+                child: Text('Close'),
+              ),
+              ElevatedButton(
+                onPressed: _handleFlushHive,
+                child: Text('Reset'),
+              ),
+            ],
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: OutlinedButton(
-          onPressed: _handleFlushHive, child: Text('Clear Saved Data')),
+          onPressed: _showDeleteAllDialog, child: Text('Clear Saved Data')),
     );
   }
 }
