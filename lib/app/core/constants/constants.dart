d1e717365dec06d80c228b480cf24ee4dda7a9c7
KendallRey <107897863+KendallRey@users.noreply.github.com>
2025-02-06 16:02:16 +0800

    DATA - refactor dir and init Hive

---
+++ lib/app/core/constants/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static String hiveBox = '@qr-scanner-box';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive/hive.dart';
 import 'package:qr_scanner/app/app.dart';
 import 'package:qr_scanner/app/observers/app_life.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  final path = Directory.current.path;
+  Hive.init(path);
   runApp(AppLifecycleObserver(child: MyApp()));
 }
