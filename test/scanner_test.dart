9a1f23f608bc6ba4d276b8d56b169076cd3f979c
KendallRey <107897863+KendallRey@users.noreply.github.com>
2025-02-10 12:59:35 +0800

    TEST - remove unused imports

--- test/scanner_test.dart
+++ test/scanner_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:qr_scanner/app/app.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_test/hive_test.dart';
 import 'package:qr_scanner/app/core/constants/constants.dart';
