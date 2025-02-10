14af009b06169f7a91b99669198c1864102eb704
KendallRey <107897863+KendallRey@users.noreply.github.com>
2025-02-10 12:53:29 +0800

    TEST - added initial test files and package

---
+++ test/settings_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_scanner/app/app.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_test/hive_test.dart';
+import 'package:qr_scanner/app/core/constants/constants.dart';
+import 'package:qr_scanner/app/core/providers/app_settings_provider.dart';
+import 'package:qr_scanner/app/router.dart';
+
+void main() {
+  late Box testBox;
+
+  setUp(() async {
+    await setUpTestHive();
+    testBox = await Hive.openBox(Constants.hiveBox);
+  });
+
+  tearDown(() async {
+    await testBox.close();
+  });
+
+  testWidgets('Navigate to Settings Screen', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => AppSettingsProvider()),
+        ],
+        child: MaterialApp.router(
+          routerConfig: AppRouter().router,
+        ),
+      ),
+    );
+
+    await tester.tap(find.byIcon(Icons.settings));
+    await tester.pumpAndSettle();
+    expect(find.text("Settings"), findsExactly(2));
+
+    await tester.tap(find.byIcon(Icons.home));
+    await tester.pumpAndSettle();
+    expect(find.text("Home"), findsExactly(2));
+
+    await tester.tap(find.byIcon(Icons.qr_code_scanner));
+    await tester.pumpAndSettle();
+    expect(find.text("Scanner"), findsExactly(2));
+
+    await tester.tap(find.byIcon(Icons.settings));
+    await tester.pumpAndSettle();
+    expect(find.text("Settings"), findsExactly(2));
+  });
+}
